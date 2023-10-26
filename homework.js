c2f61d91de7c3efa0456c97556581f5cc5933144
sahomelabs <sahomelabs@gmail.com>
2023-10-26 11:04:52 -0400

    created person class

--- homework.js
+++ homework.js
@@ -18,3 +18,54 @@ let myHamster= new Hamster('Buddy');
 myHamster.wheelRun();
 myHamster.eatFood(); 
 console.log(myHamster.getPrice()); 
+
+
+class Person {
+    constructor (name) {
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.Hamster = [];
+        this.bankAccount = 0;
+    }
+    getName() {
+        return this.name;
+    }
+    getAge(){
+        return this.age;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    greet() {
+        console.log(`Hello my name is ${this.name}`);
+    }
+    eat(){
+        this.weight++;
+        this.mood++;
+    }
+    exercise(){
+        this.weight--;
+    }
+    ageUP() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount +=10
+    }
+    buyHamster (hamster) {
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= hamster.getPrice();
+        }
+
+    }
+    const person = new Person("Dan");
+    person.greet();
+    person.eat();
+    person.exercise();
+    person.ageUP();
+    console.log(person);
